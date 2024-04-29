beae991ea36e72d1abe20c15f8ae5efe37177de7
JatinKumar1103 <kumarjatin1103@gmail.com>
2024-04-30 01:41:49 +0530

    Divide and Conquer

--- Divide And Conquer/SortedAndRotated.java
+++ Divide And Conquer/SortedAndRotated.java
@@ -26,5 +26,7 @@ public class SortedAndRotated {
     public static void main(String[] args) {
         int arr[] ={ 4,5,6,7,0,1,2};
         System.out.println(search(arr, 0 , arr.length-1, 0));
+
     }
+    
 }
